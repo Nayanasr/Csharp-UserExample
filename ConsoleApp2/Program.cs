f6acf5ad1cf5d7f84162c5e45088014893380e61
Nayana Ramesh <107012@tfcorp.com>
2022-11-09 19:41:31 +0530

    User class added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Admin _admin = new Admin();
+            _admin.sendDocs();
+            _admin.restrict();
+        }
+    }
+}
---
+++ ConsoleApp2/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    //Whatsapp example
+   public class User
+    {
+        public void chat()
+        {
+            Console.WriteLine("I can chat");
+        }
+        public void videoCall()
+        {
+            Console.WriteLine("I can make video call");
+        }
+        public void sendDocs()
+        {
+            Console.WriteLine("I can send docs");
+        }
+        public void changeGrpName()
+        {
+            Console.WriteLine("I can change group name");
+        }
+    }
+    
+    //admin is a child class inherited from use class(base class/parent class)
+
+    public class Admin: User
+    {
+        //creating reference variable of user inside the admin class
+
+        public void restrict()
+        {
+            Console.WriteLine("I can restrict user");
+        }
+        public void addUser()
+        {
+            Console.WriteLine("I can add users");
+        }
+    }
+}
